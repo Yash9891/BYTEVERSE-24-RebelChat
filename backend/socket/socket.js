bf33a45a7ccbbfc686876bc484c5b89554b0d38c
Yash9891 <yash042002@gmail.com>
2024-04-14 16:28:12 +0530

    j

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,38 +1,40 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import cors from "cors"; // Import the cors middleware
 
 const app = express();
 
+// Use the cors middleware to enable CORS
+app.use(cors({
+  origin: ["https://rebel-chat.onrender.com", "http://localhost:5000"],
+  methods: ["GET", "POST"],
+}));
+
 const server = http.createServer(app);
-const io = new Server(server, {
-	cors: {
-		origin: ["https://rebel-chat.onrender.com"],
-		methods: ["GET", "POST"],
-	},
-});
+const io = new Server(server);
 
 export const getReceiverSocketId = (receiverId) => {
-	return userSocketMap[receiverId];
+  return userSocketMap[receiverId];
 };
 
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
-	console.log("a user connected", socket.id);
+  console.log("a user connected", socket.id);
 
-	const userId = socket.handshake.query.userId;
-	if (userId != "undefined") userSocketMap[userId] = socket.id;
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
 
-	// io.emit() is used to send events to all the connected clients
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-	// socket.on() is used to listen to the events. can be used both on client and server side
-	socket.on("disconnect", () => {
-		console.log("user disconnected", socket.id);
-		delete userSocketMap[userId];
-		io.emit("getOnlineUsers", Object.keys(userSocketMap));
-	});
+  // socket.on() is used to listen to the events. can be used both on client and server side
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
 });
 
 export { app, io, server };
